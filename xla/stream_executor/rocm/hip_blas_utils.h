87bfd83fc2d3fb88da24dbedfc50f10734446fdd
Eugene Kuznetsov <eugene.kuznetsov@amd.com>
2023-10-22 23:10:41 +0000

    Changes to hipblaslt datatypes in rocm 6.0

--- xla/stream_executor/rocm/hip_blas_utils.h
+++ xla/stream_executor/rocm/hip_blas_utils.h
@@ -30,7 +30,11 @@ namespace rocm {
   TF_RETURN_IF_ERROR(::stream_executor::rocm::ToStatus(expr, #expr))
 
 tsl::Status ToStatus(hipblasStatus_t status, const char* prefix);
+#if HIP_VERSION_MAJOR >= 6
+hipblasltDatatype_t AsHipblasDataType(blas::DataType type);
+#else
 hipblasDatatype_t AsHipblasDataType(blas::DataType type);
+#endif
 hipblasLtComputeType_t AsHipblasComputeType(blas::ComputationType type);
 hipblasOperation_t AsHipblasOperation(blas::Transpose trans);
 
