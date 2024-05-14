f0bebcf9f933fef7d1ee9fa74c9b2b4e9709fbe5
Rahul Batra <rahbatra@amd.com>
2024-05-14 18:59:37 +0000

    Cost model updates

--- xla/service/gpu/model/gpu_performance_model_base.cc
+++ xla/service/gpu/model/gpu_performance_model_base.cc
@@ -68,7 +68,7 @@ int GetCoalescingWasteFactor(PrimitiveType element_type) {
   // configured.
   // https://developer.download.nvidia.com/video/gputechconf/gtc/2020/presentations/s21819-optimizing-applications-for-nvidia-ampere-gpu-architecture.pdf
   // (page 10).
-  constexpr int kDRAMToL2TransactionSizeBytes = 64;
+  constexpr int kDRAMToL2TransactionSizeBytes = 128;
   // Assume we use one element from the cache line and waste the remaining
   // bandwidth. For example, if we're reading f32s, we use 1/16nd of the cache
   // line.
@@ -80,7 +80,7 @@ int GetCoalescingWasteFactor(PrimitiveType element_type) {
 // (1830 MHz) to saturate the memory bandwidth (3.35 TB/s).
 float AdjustBandwidth(const se::DeviceDescription& gpu_device_info,
                       float bandwidth, int64_t num_blocks) {
-  float per_block_bandwidth = gpu_device_info.clock_rate_ghz() * 1.0e9f * 32;
+  float per_block_bandwidth = gpu_device_info.clock_rate_ghz() * 1.0e9f * 16;
   float max_bandwidth = num_blocks * per_block_bandwidth;
 
   return std::min(bandwidth, max_bandwidth);
