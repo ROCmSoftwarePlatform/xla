9aadc4d1eafa2d89bfc7c74acf5d344fe9bc90c6
Zoran Jovanovic <zjovanov@amd.com>
2024-05-30 15:36:33 +0000

    [ROCm] Fix an issue with Softmax.

--- xla/service/gpu/gpu_compiler.cc
+++ xla/service/gpu/gpu_compiler.cc
@@ -1453,8 +1453,9 @@ absl::Status GpuCompiler::OptimizeHloPostLayoutAssignment(
     // ReductionDimensionGrouper, as that makes matching the softmax pattern
     // harder.
     if (debug_options.xla_gpu_enable_triton_softmax_fusion() &&
-        cuda_cc != nullptr &&
-        cuda_cc->IsAtLeast(se::CudaComputeCapability::AMPERE)) {
+        ((cuda_cc != nullptr &&
+        cuda_cc->IsAtLeast(se::CudaComputeCapability::AMPERE)) ||
+        rocm_cc != nullptr)) {
       pipeline.AddPass<HloPassFix<AlgebraicSimplifier>>(simplifier_options);
       pipeline.AddPass<SoftmaxRewriterTriton>(gpu_version);
     }
