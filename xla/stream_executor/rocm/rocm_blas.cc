98994ec45ccadf1e84491d18c540a34dbea16627
Pavel Emeliyanenko <pavel.emeliyanenko@amd.com>
2024-08-22 03:35:24 -0500

    disable workspace

--- xla/stream_executor/rocm/rocm_blas.cc
+++ xla/stream_executor/rocm/rocm_blas.cc
@@ -366,12 +366,19 @@ absl::Status ROCMBlas::DoBlasInternalImpl(FuncT rocblas_func, Stream *stream,
                  << ": " << ToString(ret);
     }
   }
-#if TF_ROCM_VERSION >= 60000
-  if (auto *workspace = GetWorkspace(); workspace != nullptr &&
-                                        workspace->opaque() != nullptr &&
-                                        workspace->size() > 0) {
-    (void)wrap::rocblas_set_workspace(blas_, workspace->opaque(),
-                                      workspace->size());
+#if 0
+// pemeliya: the feature is disabled since rocblas does not perform well under
+// graph capture. rocblas_set_workspace seems to use blocking memory functions
+// like hipFree/hipMalloc which result in HIP_ERROR_StreamCaptureUnsupported
+  {
+    auto *workspace = GetWorkspace();
+    auto *wptr = workspace != nullptr ? workspace->opaque() : nullptr;
+    size_t wsize = workspace != nullptr ? workspace->size() : 0;
+    ret = wrap::rocblas_set_workspace(blas_, wptr, wsize);
+    if (err_on_failure && ret != rocblas_status_success) {
+      LOG(ERROR) << "failed to set workspace before " << FuncT::kName
+                 << ": " << ToString(ret);
+    }
   }
 #endif
 
