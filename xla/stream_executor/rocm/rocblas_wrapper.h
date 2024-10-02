c320e9c15c8915dd8a5e0aa56f4448bde308297c
Zoran Jovanovic <zjovanov@amd.com>
2024-10-02 14:32:37 +0000

    [ROCm] Fixed linker issues with rocblas_get_version_string_size and rocblas_get_version_string

--- xla/stream_executor/rocm/rocblas_wrapper.h
+++ xla/stream_executor/rocm/rocblas_wrapper.h
@@ -275,7 +275,9 @@ using stream_executor::internal::CachedDsoLoader::GetRocblasDsoHandle;
   __macro(rocblas_destroy_handle)                        \
   __macro(rocblas_get_stream)                            \
   __macro(rocblas_set_stream)                            \
-  __macro(rocblas_set_atomics_mode)
+  __macro(rocblas_set_atomics_mode)                      \
+  __macro(rocblas_get_version_string_size)               \
+  __macro(rocblas_get_version_string)
 
 // clang-format on
 
