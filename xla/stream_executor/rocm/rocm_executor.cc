ca02536459dd90769924a82a9377cfab4d72f274
Pavel Emeliyanenko <pavel.emeliyanenko@amd.com>
2025-01-20 13:48:04 +0000

    Avoid lazy init of blas handles and fix for non-canonical dots

--- xla/stream_executor/rocm/rocm_executor.cc
+++ xla/stream_executor/rocm/rocm_executor.cc
@@ -237,7 +237,20 @@ absl::Status GpuExecutor::Init() {
     return status;
   }
 
-  return GpuDriver::GetGpuISAVersion(&version_, device_);
+  status = GpuDriver::GetGpuISAVersion(&version_, device_);
+  if (!status.ok()) {
+    return status;
+  }
+  // We initialize BLAS interfaces early here since otherwise it might create 
+  // us problems during hipBlasLt initialization under graph capture.
+  // There is no real advantage of explicitly using 'lazy initialization' on 
+  // ROCM platform because rocBLAS/hipBlasLt already use 'lazy initialization' 
+  // internally
+  PluginRegistry* registry = PluginRegistry::Instance();
+  TF_ASSIGN_OR_RETURN(auto factory, 
+      registry->GetFactory<PluginRegistry::BlasFactory>(rocm::kROCmPlatformId));
+  blas_.reset(factory(this));
+  return absl::OkStatus();
 }
 
 // Returns the path to the running executable.
@@ -681,22 +694,6 @@ absl::Status GpuExecutor::BlockHostUntilDone(Stream* stream) {
 }
 
 blas::BlasSupport* GpuExecutor::AsBlas() {
-  absl::MutexLock lock(&mu_);
-  if (blas_ != nullptr) {
-    return blas_.get();
-  }
-
-  PluginRegistry* registry = PluginRegistry::Instance();
-  absl::StatusOr<PluginRegistry::BlasFactory> status =
-      registry->GetFactory<PluginRegistry::BlasFactory>(rocm::kROCmPlatformId);
-  if (!status.ok()) {
-    LOG(ERROR) << "Unable to retrieve BLAS factory: "
-               << status.status().message();
-    return nullptr;
-  }
-
-  auto blas = status.value()(this);
-  blas_.reset(blas);
   return blas_.get();
 }
 
